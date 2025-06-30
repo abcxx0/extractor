96956a60d786c3c54e862fbaae77966dc891015d
abcxx0 <abramcastroo@gmail.com>
2025-06-30 14:02:28 -0300

    Update enviar_newsletter.py

--- enviar_newsletter.py
+++ enviar_newsletter.py
@@ -47,7 +47,8 @@ SMTP_HOST = "smtp.gmail.com"
 SMTP_PORT = 465
 
 # ---------- Destinatarios y asunto ----------
-TO = ["reimundo.m52@gmail.com"]             # ← destinatario(s) fijos
+TO = ["reimundo.m52@gmail.com",
+  "diariociudadanosur@hotmail.com]             # ← destinatarios fijos
 SUBJECT = "📊 Informe semanal 📈"
 
 # ---------- Construir mensaje ----------
